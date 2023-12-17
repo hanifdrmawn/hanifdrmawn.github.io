cc6a7435c524567539d44aa9e3c77e17dabba59d
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-12-17 15:50:26 +0700

    fix bug mobile view

--- Project/script.js
+++ Project/script.js
@@ -10,6 +10,12 @@ window.onscroll = function () {
     }
 };
 
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
 function project_alert() {
     alert("Mohon Maaf, Project Tidak di Deploy.");
 }
